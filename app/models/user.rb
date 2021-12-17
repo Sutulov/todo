f5b94e605232424266164dc7cbcb4092d2ed2c6f
Sutulov <sav.xtx@gmail.com>
2021-12-17 20:47:31 +0400

    test + rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Class User
 class User < ApplicationRecord
 end
--- db/migrate/20211216165226_create_users.rb
+++ db/migrate/20211216165226_create_users.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
+# Class CreateUsers
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.boolean :active
+    create_table :users, comment: 'Пользователи системы' do |t|
+      t.string :name, comment: 'Имя пользователя', index: { unique: true }
+      t.string :email, comment: 'Электронный адрес пользователя', index: { unique: true }
+      t.boolean :active,
+                default: true,
+                comment: 'пользователь активен (true) или забанен (false)'
 
       t.timestamps
     end
-    add_index :users, :name, unique: true
-    add_index :users, :email, unique: true
   end
 end
