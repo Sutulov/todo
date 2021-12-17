f88885166413cc6bf3e3e8d3ed4b79200c63aaec
Sutulov <sav.xtx@gmail.com>
2021-12-17 21:07:00 +0400

    test validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@
 
 # Class User
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { maximum: 16, minimum: 2 }
 end
