ae592495e25814a68f82b63e7cc563b0f0ef18a8
Sutulov <sav.xtx@gmail.com>
2021-12-19 19:53:31 +0400

    test

--- db/migrate/20211218080022_create_items.rb
+++ db/migrate/20211218080022_create_items.rb
@@ -1,6 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
-    create_table :items do |t|
+    create_table :items, comment: 'Подпункты дела' do |t|
+      t.string :name, comment: 'Заголовок'
+      t.boolean :done, default: false,
+                comment: 'Статус: завершенно (true), или нет (false)'
+      t.datetime :finished_at, comment: 'Дата и время завершения Подпункта'
+      t.references :event, foreign_key: true,
+                    comment: 'Внешний ключ для связи с таблицей events'
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,17 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_216_165_226) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_12_19_154158) do
 
-  create_table 'users', comment: 'Пользователи системы', force: :cascade do |t|
-    t.string 'name', comment: 'Имя пользователя'
-    t.string 'email', comment: 'Электронный адрес пользователя'
-    t.boolean 'active', default: true, comment: 'пользователь активен (true) или забанен (false)'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['name'], name: 'index_users_on_name', unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", comment: "Список дел", force: :cascade do |t|
+    t.string "name", comment: "Заголовок"
+    t.text "content", comment: "Детальное описание"
+    t.boolean "done", default: false, comment: "Статус: завершенно (true), или нет (false)"
+    t.datetime "finished_at", comment: "Дата и время завершения дела"
+    t.bigint "user_id", comment: "Внешний ключ для связи с таблицей user"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
+
+  create_table "items", comment: "Подпункты дела", force: :cascade do |t|
+    t.string "name", comment: "Заголовок"
+    t.boolean "done", default: false, comment: "Статус: завершенно (true), или нет (false)"
+    t.datetime "finished_at", comment: "Дата и время завершения Подпункта"
+    t.bigint "event_id", comment: "Внешний ключ для связи с таблицей events"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_items_on_event_id"
+  end
+
+  create_table "roles", comment: "Роли пользователя", force: :cascade do |t|
+    t.string "name", comment: "Заголовок"
+    t.string "code", comment: "Псевдоним"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", comment: "Пользователи системы", force: :cascade do |t|
+    t.string "name", comment: "Имя пользователя"
+    t.string "email", comment: "Электронный адрес пользователя"
+    t.boolean "active", default: true, comment: "пользователь активен (true) или забанен (false)"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "role_id", comment: "Роль пользователя"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
+
+  add_foreign_key "events", "users"
+  add_foreign_key "items", "events"
+  add_foreign_key "users", "roles"
 end
