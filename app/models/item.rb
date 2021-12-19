b76a95056c0a3fc108909a92bbfe6dc3f1f89d5d
Sutulov <sav.xtx@gmail.com>
2021-12-19 20:08:15 +0400

    validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
+# Class Item
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 10, minimum: 2 }
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,7 @@
+# Class Role
 class Role < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 10, minimum: 2 }
+  validates :code, presence: true
+  validates :code, length: { maximum: 10, minimum: 2 }
 end
