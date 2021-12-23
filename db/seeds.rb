118ab0b456f3afda2e23421d15940d278ae002c7
Sutulov <sav.xtx@gmail.com>
2021-12-23 21:18:38 +0400

    filling users and events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Event.destroy_all
+User.destroy_all
+
+hash_users = 10.times.map do
+  {
+    email: FFaker::Internet.safe_email,
+    name: FFaker::Internet.user_name
+  }
+end
+
+users = User.create hash_users
+
+hash_events = 20.times.map do
+  {
+    name: FFaker::Lorem.phrase,
+    content: FFaker::Lorem.paragraph,
+    user_id: users.sample.id
+  }
+end
+
+Event.create hash_events
