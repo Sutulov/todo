18c980bc177ee16fbe6f417128148a0a02bb4ec4
Sutulov <sav.xtx@gmail.com>
2021-12-29 21:58:01 +0400

    test

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,6 @@ class Event < ApplicationRecord
   validates :content, length: { maximum: 450, minimum: 2 }
   validates :name, presence: true
   validates :name, length: { maximum: 200, minimum: 2 }
+
+  belongs_to :user
 end
