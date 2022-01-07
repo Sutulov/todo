8ae3410629b30db3022a2f64518409bbe12268cd
Sutulov <sav.xtx@gmail.com>
2022-01-08 00:13:15 +0400

    use scaffold_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :events
   get 'help', to: 'help#index', as: 'help_index'
   get 'about', to: 'about#index', as: 'about_index'
 
   get 'about/other', to: 'about#index'
   get 'about/:hello', to: 'about#index'
 
-  get 'events/index', to: redirect(status: 302, path: '/')
-  root 'events#index'
+   root 'events#index'
 end
