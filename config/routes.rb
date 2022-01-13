5145a5e07111f25945c4a4dd309f2c66f292e533
Sutulov <sav.xtx@gmail.com>
2022-01-13 19:13:14 +0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :events
+  get 'events/page/(:page(.:format))', to: 'events#index'
   get 'help', to: 'help#index', as: 'help_index'
   get 'about', to: 'about#index', as: 'about_index'
 
