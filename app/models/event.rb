9e0931010773f41cf4de74e41ec834de0b78a74e
Sutulov <sav.xtx@gmail.com>
2022-01-19 20:41:05 +0400

    homework for lesson 9

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,4 +10,17 @@ class Event < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :comments, as: :commentable
   has_many :commentators, through: :comments, source: :user
+
+  after_create :count_events
+  after_destroy :recount_events
+
+  private
+
+  def count_events
+    user.increment!(done? ? :events_done_count : :events_not_done_count)
+  end
+
+  def recount_events
+    user.decrement!(done? ? :events_done_count : :events_not_done_count)
+  end
 end
