ef865612fc1e81a9601a41bcbbaac1231001c2f1
Sutulov <sav.xtx@gmail.com>
2022-02-12 18:06:31 +0500

    add gem 'simplecov'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,35 +6,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_length_of(:name).is_at_least(2) }
   it { is_expected.to validate_length_of(:name).is_at_most(25) }
-  it { is_expected.to validate_uniqueness_of(:name) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-
-  # validates :email, :name, presence: true
-  # validates :email, :name, uniqueness: true
-  # validates :name, length: { maximum: 25, minimum: 2 }
-
-  # before :each do
-  #   Role.create(name: 'Пользователь', code: :default)
-  # end
-
-  # context 'is valid' do
-  #   let(:user) { FactoryBot.create :user }
-
-  #   it 'with valid attributes' do
-  #     expect(user).to be_valid
-  #   end
-  # end
-
-  # context 'is not valid' do
-  #   let(:user_short_name) { FactoryBot.build :user, name: 'a' }
-  #   let(:user_long_name) { FactoryBot.build :user, name: 'a' * 30 }
-
-  #   it 'with a too short name' do
-  #     expect(user_short_name).to_not be_valid
-  #   end
-
-  #   it 'with a too long name' do
-  #     expect(user_long_name).to_not be_valid
-  #   end
-  # end
+#   it { is_expected.to validate_uniqueness_of(:name) }
+#   it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to belong_to(:role) }
 end
